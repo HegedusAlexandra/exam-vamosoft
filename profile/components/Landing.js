c77e1d1fbe0b4adda467a2f11aacd5ffa9e1334b
HegedusAlexandra <hexapastry@gmail.com>
2023-04-05 10:52:30 +0200

    added tailwind styles

--- profile/components/Landing.js
+++ profile/components/Landing.js
@@ -1,138 +1,131 @@
 import { Text, View, Button, Image, Pressable } from "react-native";
 import { Stack, IconButton } from "@react-native-material/core";
-
+import { styled } from "nativewind";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-
-import tw from "twrnc";
 import SvgComponent from "./Svg";
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledImage = styled(Image);
+const StyledPressable = styled(Pressable);
+const StyledIconButton = styled(IconButton);
 
 export default function Landing({ navigation }) {
   return (
-    <View style={tw`flex-col w-full h-full bg-white`}>
-      <View style={tw`w-100 h-3/20 bg-[#006EE9] justify-center `}>
-        <Text
-          style={tw`text-md text-white dark:text-white m-4 font-black text-lg tracking-wide`}
-        >
+    <StyledView className="flex-col bg-white">
+      <StyledView className="w-full h-1/6 bg-[#006EE9] justify-center">
+        <StyledText className="text-md text-white m-4 font-black text-lg tracking-wide">
           Profile
-        </Text>
-      </View>
+        </StyledText>
+      </StyledView>
       <SvgComponent></SvgComponent>
-      <View style={tw`w-full h-16/20 bg-white bottom-0 `}>
-        <View
-          style={tw`absolute z-1 left-1/20 -top-5/20 w-9/10 h-1/4 bg-white items-center justify-center rounded-lg shadow-lg shadow-blue-500/100`}
-        >
-          <Image
+      <StyledView className="w-full h-4/6 bg-white items-center justify-stretch">
+        <StyledView className="absolute z-1 w-10/12 h-1/4 bg-white items-center justify-center rounded-lg shadow-lg shadow-blue-500/100">
+          <StyledImage
             source={require("../example.jpg")}
-            style={tw`w-20 h-20 rounded-full -top-1/10`}
-          ></Image>
-          <Text
-            style={tw`text-md text-lg text-blue-600 mb-1 font-sans font-black tracking-wide -top-1/10`}
-          >
+            className="w-20 h-20 rounded-full -top-1/10"
+          ></StyledImage>
+          <StyledText className="text-md text-lg text-blue-600 mb-1 font-black tracking-wide -top-1/10">
             Philipp Williamson
-          </Text>
-          <Text
-            style={tw`text-md text-black font-sans italic tracking-wide -top-1/10`}
-          >
+          </StyledText>
+          <StyledText className="text-md text-black italic tracking-wide -top-1/10">
             UI UX Designer
-          </Text>
-          <View style={tw`flex-row justify-evenly w-full mb-5`}>
-            <View style={tw`flex-row justify-start items-center w-1/2`}>
-              <IconButton
+          </StyledText>
+          <StyledView className="flex-row justify-evenly w-full mb-5">
+            <StyledView className="flex-row justify-start items-center w-1/2">
+              <StyledIconButton
                 icon={(props) => <Icon name="map-marker" {...props} />}
                 color="#006EE9"
                 container
               />
-              <Text style={tw`text-xs text-black font-sans `}>
+              <StyledText className="text-xs text-black">
                 Malang,Indonesia
-              </Text>
-            </View>
-            <View style={tw`flex-row justify-start items-center w-1/2 `}>
-              <IconButton
+              </StyledText>
+            </StyledView>
+            <StyledView className="flex-row justify-start items-center w-1/2">
+              <StyledIconButton
                 icon={(props) => <Icon name="bag-checked" {...props} />}
                 color="#006EE9"
                 size="small"
               />
-              <Text style={tw`text-xs text-black font-sans `}>
+              <StyledText className="text-xs text-black">
                 2565 Task completed
-              </Text>
-            </View>
-          </View>
-        </View>
-        <Pressable
+              </StyledText>
+            </StyledView>
+          </StyledView>
+        </StyledView>
+        <StyledPressable
           onPress={() => navigation.navigate("My Profile")}
           title="My profile"
-          style={tw`w-full h-20 flex-row items-center ml-10`}
+          className="w-full h-20 flex-row items-center ml-10"
         >
-          <IconButton
+          <StyledIconButton
             icon={(props) => <Icon name="account" {...props} />}
             color="#006EE9"
           />
-          <Text style={tw`ml-8`}>My Profile</Text>
-        </Pressable>
-        <Pressable
+          <StyledText className="ml-8">My Profile</StyledText>
+        </StyledPressable>
+        <StyledPressable
           title="Statistic"
-          style={tw`w-full h-20 flex-row items-center ml-10`}
+          className=" h-20 flex-row items-center ml-10"
         >
-          <IconButton
+          <StyledIconButton
             icon={(props) => <Icon name="chart-box" {...props} />}
             color="#006EE9"
             container
           />
 
-          <Text style={tw`ml-8`}>Statistic</Text>
-        </Pressable>
-        <Pressable
+          <StyledText className="ml-8">Statistic</StyledText>
+        </StyledPressable>
+        <StyledPressable
           title="Location"
-          style={tw`w-full h-20 flex-row items-center ml-10`}
+          className="w-full h-20 flex-row items-center ml-10"
         >
-          <IconButton
+          <StyledIconButton
             icon={(props) => <Icon name="map-marker" {...props} />}
             color="#006EE9"
           />
 
-          <Text style={tw`ml-8`}>Location</Text>
-        </Pressable>
-        <Pressable
+          <StyledText className="ml-8">Location</StyledText>
+        </StyledPressable>
+        <StyledPressable
           title="Settings"
-          style={tw`w-full h-20 flex-row items-center ml-10`}
+          className="w-full h-20 flex-row items-center ml-10"
         >
-          <IconButton
+          <StyledIconButton
             icon={(props) => <Icon name="cog" {...props} />}
             color="#006EE9"
           />
 
-          <Text style={tw`ml-8`}>Settings</Text>
-        </Pressable>
-        <Pressable
+          <StyledText className="ml-8">Settings</StyledText>
+        </StyledPressable>
+        <StyledPressable
           title="Logout"
-          style={tw`w-full h-20 flex-row items-center ml-10`}
+          className="w-full h-20 flex-row items-center ml-10"
         >
-          <IconButton
+          <StyledIconButton
             icon={(props) => <Icon name="logout" {...props} />}
             color="#006EE9"
             container
           />
 
-          <Text style={tw`ml-8`}>Logout</Text>
-        </Pressable>
-        <View
-          style={tw`absolute flex-row bottom-28 w-full h-3/24 bg-white items-center justify-evenly shadow-lg shadow-blue-800/100 `}
-        >
-          <IconButton
+          <StyledText className="ml-8">Logout</StyledText>
+        </StyledPressable>
+        <StyledView className="absolute flex-row bottom-28 w-full h-3/24 bg-white items-center justify-evenly shadow-lg shadow-blue-800/100">
+          <StyledIconButton
             icon={(props) => <Icon name="home-variant" {...props} />}
             color="#006EE966"
           />
-          <IconButton
+          <StyledIconButton
             icon={(props) => <Icon name="calendar-month" {...props} />}
             color="#006EE966"
           />
-          <IconButton
-            style={tw``}
+          <StyledIconButton
             icon={(props) => <Icon name="account" {...props} />}
             color="#006EE9"
           />
-        </View>
-      </View>
-    </View>
+        </StyledView>
+      </StyledView>
+    </StyledView>
   );
 }
--- profile/components/Myprofile.js
+++ profile/components/Myprofile.js
@@ -1,82 +1,78 @@
-import { Text, View, TextInput, Image, Pressable } from "react-native";
-import Svg, { Circle, Rect } from "react-native-svg";
-import { Stack, IconButton } from "@react-native-material/core";
+import { Text, View, TextInput, Image, Pressable, Button } from "react-native";
+import { IconButton } from "@react-native-material/core";
 
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import tw from "twrnc";
+
 import { useQuery } from "@apollo/client";
 import { CONTINENT_QUERY } from "../gql/Query";
 
+import { styled } from "nativewind";
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledImage = styled(Image);
+
+console.log(Icon);
 export default function Myprofile({ navigation }) {
-  const textInputStyle = tw`h-10 w-84 p-3 rounded-md border border-blue-100`;
-  const textLabelStyle = tw`text-blue-500 mt-5 mb-1 font-bold text-lg`;
+  const textInputStyle =
+    "h-11 w-11/12 p-2 bg-white shadow-sm shadow-blue-500/50 rounded-xl border border-blue-50 ";
+  const textLabelStyle =
+    "h-7 w-11/12  mt-4 text-blue-500 font-semibold text-lg";
   const { data, loading } = useQuery(CONTINENT_QUERY);
 
   if (loading) {
-    console.log("loading started");
+    /* console.log("loading started"); */
     return <Text>loading</Text>;
   }
-  console.log(data);
+  /*   console.log(data); */
 
   return (
-    <View style={tw` w-full h-full`}>
-      <View
-        style={tw`flex-row w-full h-18/80 bg-[#006EE9] items-center justify-center`}
-      >
-        <View
-          style={tw`absolute left-4 w-10 h-10 bg-white rounded-lg items-center justify-center`}
-        >
+    <StyledView className="w-full h-full bg-[#006EE9] flex-col">
+      <StyledView className="flex-row items-center justify-center w-full h-1/6 pt-10 bg-[#006EE9]">
+        <StyledView className="absolute left-4 top-20 items-center justify-center w-8 h-8 bg-white rounded-lg ">
           <IconButton
             icon={(props) => <Icon name="arrow-left" {...props} />}
             onPress={() => navigation.navigate("Landing")}
             color="#006EE9"
           />
-        </View>
-        <Text
-          style={tw`text-md text-white dark:text-white font-bold text-lg tracking-wide`}
-        >
+        </StyledView>
+        <StyledText className="text-md text-white dark:text-white font-bold text-lg tracking-wide">
           My Profile
-        </Text>
-      </View>
-      <View
-        style={tw`absolute z-1 -bottom-12 w-full h-36/40 bg-white rounded-[14] items-center justify-start pt-10`}
-      >
-        <View style={tw` h-1/6 flex-row items-start justify-center`}>
-          <Image
+        </StyledText>
+      </StyledView>
+      <StyledView className="w-full h-5/6 bg-white rounded-t-[50] items-center justify-start pt-8">
+        <StyledView className="h-1/6 flex-row items-start justify-center">
+          <StyledImage
             source={require("../example.jpg")}
-            style={tw`w-24 h-24 rounded-full `}
-          ></Image>
-          <View
-            style={tw`absolute right-0 bottom-2 w-8 h-8 bg-white rounded-lg items-center justify-center border-2 border-blue-500`}
-          >
+            className="w-20 h-20 rounded-full"
+          ></StyledImage>
+          <StyledView className="absolute right-0 bottom-2 w-8 h-8 bg-white rounded-lg items-center justify-center border-2 border-blue-500">
             <IconButton
               icon={(props) => <Icon name="pencil" {...props} />}
               color="#006EE9"
             />
-          </View>
-        </View>
-        <View style={tw`m-4`}>
-          <Text style={textLabelStyle}>Name</Text>
-          <TextInput style={textInputStyle} value={data.user.name} />
-          <Text style={textLabelStyle}>Profession</Text>
-          <TextInput style={textInputStyle} value={data.user.profession} />
-          <Text style={textLabelStyle}>Date of Birth</Text>
-          <TextInput
-            style={textInputStyle}
-            value={new Intl.DateTimeFormat("en-US").format(
-              Date.parse(data.user.dateOfBirth)
-            )}
-          />
-          <Text style={textLabelStyle}>Email</Text>
-          <TextInput style={textInputStyle} value={data.user.email} />
-          <Pressable
-            title="Save"
-            style={tw`h-10 w-84 bg-blue-500 items-center justify-center mt-16 rounded-md `}
-          >
-            <Text style={tw`text-white font-bold text-lg`}>Save</Text>
-          </Pressable>
-        </View>
-      </View>
-    </View>
+          </StyledView>
+        </StyledView>
+        <StyledText className={textLabelStyle}>Name</StyledText>
+        <TextInput className={textInputStyle} value={data.user.name} />
+        <StyledText className={textLabelStyle}>Profession</StyledText>
+        <TextInput className={textInputStyle} value={data.user.profession} />
+        <StyledText className={textLabelStyle}>Date of Birth</StyledText>
+        <TextInput
+          className={textInputStyle}
+          value={new Intl.DateTimeFormat("en-US").format(
+            Date.parse(data.user.dateOfBirth)
+          )}
+        />
+        <StyledText className={textLabelStyle}>Email</StyledText>
+        <TextInput className={textInputStyle} value={data.user.email} />
+        <Pressable
+          title="Save"
+          className="h-11 w-11/12 bg-blue-500 items-center justify-center mt-14 rounded-xl"
+        >
+          <Text className="text-white font-semibold text-lg">Save</Text>
+        </Pressable>
+      </StyledView>
+    </StyledView>
   );
 }
